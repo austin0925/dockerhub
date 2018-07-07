d6405b2d013a51b7743dd7fc8ef716606c530061
austinmsn <austinmsn@hotmail.com>
2018-07-07 14:13:14 +0800

    runner script - update

---
+++ tomcatRunner/register.sh
@@ -0,0 +1,1 @@
+docker run --rm -t -i -v /path/to/config:/etc/gitlab-runner --name gitlab-runner gitlab/gitlab-runner register
---
+++ tomcatRunner/startRunner.sh
@@ -0,0 +1,1 @@
+docker run --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -v /path/to/config:/etc/gitlab-runner --name gitlab-runner gitlab/gitlab-runner
