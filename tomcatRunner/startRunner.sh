4dc5042b40e96e759df7bbb27df7347962d299e2
austin-wang <austin-wang@nec.com.tw>
2018-07-14 17:19:25 +0800

    rename to pwd

--- tomcatRunner/startRunner.sh
+++ tomcatRunner/startRunner.sh
@@ -1,1 +1,1 @@
-docker run --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -v /path/to/config:/etc/gitlab-runner --name gitlab-runner gitlab/gitlab-runner
+docker run --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/config:/etc/gitlab-runner --name gitlab-runner gitlab/gitlab-runner
